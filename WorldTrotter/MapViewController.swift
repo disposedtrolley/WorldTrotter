2f58ab39a6d6dab2955d32f57a66f64b0b778ffb
James Liu <james_liu@fastmail.com>
2017-03-22 15:11:45 +1100

    updated map view to be created programmatically

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // create a map view
+        mapView = MKMapView()
+        
+        // set it as the view of this view controller
+        view = mapView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
